0ff97675b0ff3dbbfe8d1f09d6ec070add4383de
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-03-24 11:21:37 +0000

    Start the release counting process

--- jdata.py
+++ jdata.py
@@ -12,6 +12,13 @@ def get_issue_by_key(key):
     return dbutils.execute_query(issue_sql, (key,), DATABASE_FILE)
 
 
+def get_affected_versions(issue_id):
+    version_sql = "SELECT v.* FROM Version v, Issue i ,VersionPerIssue vi " \
+                  "WHERE i.id = vi.issueId AND vi.versionId = v.id AND i.id =?"
+
+    return dbutils.execute_query(version_sql, (issue_id,), DATABASE_FILE)
+
+
 def get_project_issues(project_id):
     issue_sql = "SELECT * FROM Issue WHERE projectId=?"
     return dbutils.execute_query(issue_sql, (project_id,), DATABASE_FILE)
--- relcounter.py
+++ relcounter.py
@@ -1,10 +1,12 @@
 """
 Module for the calculation of the releases to fix field.
 """
-
+import re
+import csv
 import git
 import dbutils
 import jdata
+import gminer
 
 COMMITS_DDL = "CREATE TABLE issue_commit " \
               "(project_id TEXT, issue_key TEXT, commit_sha TEXT," \
@@ -15,7 +17,13 @@ DATABASE_FILE = "jira_github.sqlite"
 PATTERN_OPTION = "--grep="
 FORMAT_SHA_OPTION = "--pretty=%H"
 CONTAINS_OPTION = "--contains"
+
+# TODO(cgavidia): Move to JDATA module
 KEY_INDEX = 31
+ISSUE_ID_INDEX = 27
+VERSION_NAME_INDEX = 6
+SHA_INDEX = 2
+TAG_NAME_INDEX = 2
 
 
 def create_schema():
@@ -24,6 +32,7 @@ def create_schema():
 
 
 def get_tags_per_commit(repository_location, project_id):
+    # TODO(cgavidia): Refactor ALL SQL to methods. And probably to a module.
     commits_sql = "SELECT DISTINCT commit_sha FROM issue_commit WHERE project_id=?"
     commits = dbutils.execute_query(commits_sql, (project_id,), DATABASE_FILE)
 
@@ -39,6 +48,52 @@ def get_tags_per_commit(repository_location, project_id):
             dbutils.load_list(insert_tag, db_records, DATABASE_FILE)
         else:
             print "No tags found for commit: ", commit
+
+
+def get_commits_by_issue(project_id, key):
+    commit_sql = "SELECT * FROM issue_commit WHERE project_id=? AND issue_key=?"
+    return dbutils.execute_query(commit_sql, (project_id, key), DATABASE_FILE)
+
+
+def get_tags_by_commit_sha(project_id, commit_sha):
+    tag_sql = "SELECT * FROM commit_tag WHERE project_id=? AND commit_sha=?"
+    return dbutils.execute_query(tag_sql, (project_id, commit_sha), DATABASE_FILE)
+
+
+def consolidate_information(project_id):
+    project_issues = jdata.get_project_issues(project_id)
+
+    records = []
+    for issue in project_issues:
+        key = issue[KEY_INDEX]
+        issue_id = issue[ISSUE_ID_INDEX]
+        affected_versions = jdata.get_affected_versions(issue_id)
+        affected_versions = [version[VERSION_NAME_INDEX] for version in affected_versions]
+        affected_versions = sorted(affected_versions)
+        earliest_affected = affected_versions[0] if len(affected_versions) > 0 else ""
+        latest_affected = affected_versions[-1] if len(affected_versions) > 0 else ""
+
+        commits = get_commits_by_issue(project_id, key)
+        unique_tags = set()
+
+        for commit in commits:
+            tags = get_tags_by_commit_sha(project_id, commit[SHA_INDEX])
+            # Only including tags in release format
+            unique_tags.update(
+                [tag[TAG_NAME_INDEX] for tag in tags if re.match(gminer.RELEASE_REGEX, tag[TAG_NAME_INDEX])])
+
+        sorted_tags = sorted(list(unique_tags))
+        earliest_tag = sorted_tags[0] if len(sorted_tags) > 0 else ""
+
+        csv_record = (key, earliest_affected, latest_affected, len(commits), len(unique_tags), earliest_tag)
+        print csv_record
+        records.append(csv_record)
+
+    with open("Release_Counter_" + project_id + ".csv", "wb") as release_file:
+        csv_writer = csv.writer(release_file)
+        csv_writer.writerow("Issue Key", "Earliest Version", "Latest Version", "Commits", "Tags", "Earliest Tag")
+        for record in records:
+            csv_writer.writerow(record)
 
 
 def get_issues_and_commits(repository_location, project_id):
@@ -64,9 +119,10 @@ def main():
     repository_location = "C:\Users\Carlos G. Gavidia\git\cloudstack"
     project_id = "12313920"
 
-    create_schema()
-    get_issues_and_commits(repository_location, project_id)
-    get_tags_per_commit(repository_location, project_id)
+    # create_schema()
+    # get_issues_and_commits(repository_location, project_id)
+    # get_tags_per_commit(repository_location, project_id)
+    consolidate_information(project_id)
 
 
 if __name__ == "__main__":
